c5c6fd8ee3a2160c4664a31f0090281f6274f96d
Oscar <oscar15-91@hotmail.com>
2024-06-14 14:22:04 +0200

    Añadido cartaHome

---
+++ src/components/CardHome.vue
@@ -0,0 +1,33 @@
+<script>
+
+</script>
+
+<template>
+<div class="row row-cols-1 row-cols-md-1 g-4">
+  <div class="col col-1">
+    <div class="card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+     
+    </div>
+  </div>
+  
+  
+</div>
+</template>
+
+<style lang="scss" scoped>
+
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@ import Header from '../components/Header.vue'
 import Footer from '../components/Footer.vue'
 import Logo from '@/components/Logo.vue'
 import Filter from '@/components/Filter.vue'
+import CardHome from '@/components/CardHome.vue'
 
 </script>
 
@@ -13,7 +14,7 @@ import Filter from '@/components/Filter.vue'
   <h1>HOME</h1>
   <Logo></Logo>
   <Filter></Filter>
-
+  <CardHome></CardHome>
   </main>
   <Footer></Footer>
 </template>
