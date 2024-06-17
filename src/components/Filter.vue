652e782ab1d4414017ef85f128003e3dda78422f
Aitor Garcia Fernandez <aitorgarciafernandezcv@gmail.com>
2024-06-17 13:53:19 +0200

    filter fixed

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,29 +1,32 @@
-<script>
-
-</script>
+<script></script>
 
 <template>
-    <div>
-        <button type="submit"><img src="../assets/img/filter-right.svg" alt="" /></button>
-
-    </div>
+  <div>
+    <button type="submit">
+      <img src="../assets/img/filter-right.svg" alt="" />
+    </button>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-    div {
-        display: flex;
-        justify-content: flex-end;
+div {
+  display: flex;
+  justify-content: flex-end;
 
-        button {
-            background-color: rgba(217, 217, 217, 0.75);
-            border-color: #373A40;
-            border-radius: 12px;
-            width: 150px;
-            height: 40px;
-            margin-right: 20px;
-            justify-content: end;
+  button {
+    background-color: rgba(217, 217, 217, 0.75);
+    border-color: #373a40;
+    border-radius: 12px;
+    width: 150px;
+    height: 40px;
+    margin-right: 50px;
+    justify-content: end;
 
-        }
+    img {
+      height: 35px;
+      width: 50px;
+      margin-left: 90px;
     }
-
+  }
+}
 </style>
