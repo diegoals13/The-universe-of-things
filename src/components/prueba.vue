758249e5cf2fb014534d1dc6dbe14989526b2c94
Aitor Garcia Fernandez <aitorgarciafernandezcv@gmail.com>
2024-06-18 14:55:37 +0200

    api half solved

--- src/components/prueba.vue
+++ src/components/prueba.vue
@@ -1,28 +1,29 @@
 <template>
-    <div>
-      <button @click="loadCharacters">Load Characters</button>
-      <div v-if="isLoading">Loading...</div>
-      <div v-else>
-        <ul>
-          <li v-for="character in characters" q
-           :key="character.id">{{ character.name }}</li>
-        </ul>
-      </div>
+  <div>
+    <button @click="loadCharacters">Load Characters</button>
+    <div v-if="isLoading">Loading...</div>
+    <div v-else>
+      <ul>
+        <li v-for="character in characters" q :key="character.id">
+          {{ character.name }}
+        </li>
+      </ul>
     </div>
-  </template>
-  
-  <script setup>
-  import { onMounted } from 'vue'
-  import { useCharacterStore } from '@/stores/character'
-  
-  const characterStore = useCharacterStore()
-  const { characters, isLoading, get } = characterStore
-  
-  const loadCharacters = async () => {
-    await get()
-  }
-  
-  onMounted(() => {
-    loadCharacters()
-  })
-  </script>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import { useCharacterStore } from "@/stores/character";
+
+const characterStore = useCharacterStore();
+const { characters, isLoading, get } = characterStore;
+
+const loadCharacters = async () => {
+  await get();
+};
+
+onMounted(() => {
+  loadCharacters();
+});
+</script>
---
+++ src/stores/character.js
@@ -0,0 +1,27 @@
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+
+export const useCharacterStore = defineStore("character", () => {
+  const uri = import.meta.env.VITE_API_ENDPOINT_CHARACTERS;
+
+  const characters = ref([]);
+  const isLoading = ref(false);
+
+  async function get() {
+    try {
+      isLoading.value = true;
+      const response = await fetch(uri + "all.json");
+      const data = await response.json();
+
+      if (response.status == 200) {
+        characters.value = data;
+      }
+    } catch (error) {
+      throw new Error("Error loading api:" + error);
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  return { characters, isLoading, get };
+});
