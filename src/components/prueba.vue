1874491327d0e29fc47ce0dadbcb67e1fc5dc148
Oscar <oscar15-91@hotmail.com>
2024-06-18 16:03:38 +0200

    cambios prueba

--- src/components/prueba.vue
+++ src/components/prueba.vue
@@ -1,29 +1,29 @@
 <template>
-  <div>
-    <button @click="loadCharacters">Load Characters</button>
-    <div v-if="isLoading">Loading...</div>
-    <div v-else>
-      <ul>
-        <li v-for="character in characters" q :key="character.id">
-          {{ character.name }}
-        </li>
-      </ul>
+    <div>
+      <button @click="loadCharacters">Load Characters</button>
+      <div v-if="isLoading">Loading...</div>
+      <div v-else>
+        
+         <ul>
+          <li v-for="character in characters" :key="character.id">{{ character.name }}</li>
+        </ul> 
+      </div>
     </div>
-  </div>
-</template>
-
-<script setup>
-import { onMounted } from "vue";
-import { useCharacterStore } from "@/stores/character";
-
-const characterStore = useCharacterStore();
-const { characters, isLoading, get } = characterStore;
-
-const loadCharacters = async () => {
-  await get();
-};
-
-onMounted(() => {
-  loadCharacters();
-});
-</script>
+  </template>
+  
+  <script setup>
+  import { onMounted } from 'vue'
+  import { useCharacterStore } from '@/stores/character'
+  
+  const characterStore = useCharacterStore()
+  const { characters, isLoading, get } = characterStore
+  
+  const loadCharacters = async () => {
+    await get()
+  }
+  
+  onMounted(() => {
+    loadCharacters()
+  })  
+  </script>
+  
--- src/stores/character.js
+++ src/stores/character.js
@@ -1,8 +1,10 @@
-import { ref, computed } from "vue";
-import { defineStore } from "pinia";
 
-export const useCharacterStore = defineStore("character", () => {
-  const uri = import.meta.env.VITE_API_ENDPOINT_CHARACTERS;
+
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useCharacterStore = defineStore('character', () => {
+  const uri = 'https://dragonball-api.com/api/character/'; // URL correcta para la API de personajes
 
   const characters = ref([]);
   const isLoading = ref(false);
@@ -10,7 +12,7 @@ export const useCharacterStore = defineStore("character", () => {
   async function get() {
     try {
       isLoading.value = true;
-      const response = await fetch(uri + "all.json");
+      const response = await fetch(uri); // Fetch sin 'all.json' al final
       const data = await response.json();
 
       if (response.status == 200) {
