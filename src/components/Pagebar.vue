39c3b1765f255ef2120fb1c5bbb35299df6350f7
Oscar <oscar15-91@hotmail.com>
2024-06-19 14:12:32 +0200

    cambios paginacion

--- src/components/Pagebar.vue
+++ src/components/Pagebar.vue
@@ -1,8 +1,51 @@
 <script setup>
 </script>
 <template>
-<h1 class="mb-5">HOla</h1>
+    
+<nav class="pagebar "> 
+    <ul class="pagination">
+   
+    <li class="page-item"><a class="page-link text-center" href="#">1</a></li>
+    <li class="page-item"><a class="page-link text-center" href="#">2</a></li>
+    <li class="page-item"><a class="page-link text-center" href="#">3</a></li>
+    <li class="page-item"><a class="page-link text-center" href="#">4</a></li>
+    <li class="page-item"><a class="page-link text-center" href="#">5</a></li>
+    <li class="page-item"><a class="page-link text-center" href="#">6</a></li>
+    <li class="page-item"><a class="page-link text-center" href="#">7</a></li>
+    <li class="page-item"><a class="page-link text-center" href="#">8</a></li>
+
+  
+  </ul>
+</nav>
+
 </template>
 <style  scoped lang="scss" >
-
+.pagebar{
+    width: 100%;
+    height: 50px;
+   
+    
+    
+    
+    ul{
+        justify-content: center;
+        
+      
+    }
+    li{
+       
+    }
+   a{
+    width: 80px;
+    color:white;
+    background-color: rgba(104, 109, 118, 0.8);
+    
+   }
+   a:hover{
+    transition-duration: 0.1ms !important;
+    color:#ffb800;
+    
+    
+   }
+}
 </style>
